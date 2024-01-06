74e6a8d62d91dea9997b87e72019451b500b42f8
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-06 14:59:46 +0530

    import java.util.*; public class Profit{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         Double x=read.nextDouble();         System.out.printf("%.2f",(x*1000)/3600);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Profit{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        Double x=read.nextDouble();
+        System.out.printf("%.2f",(x*1000)/3600);
+    }
+}
