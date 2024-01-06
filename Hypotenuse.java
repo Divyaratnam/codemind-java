ef282900a351937283987cea8d6bd70e20972cfc
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-06 11:05:40 +0530

    import java.util.*; public class Divya{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         double a=read.nextDouble();         double b=read.nextDouble();         System.out.printf("%.2f",Math.sqrt(Math.pow(a,2)+Math.pow(b,2)));     } }

---
+++ Hypotenuse.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Divya{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        double a=read.nextDouble();
+        double b=read.nextDouble();
+        System.out.printf("%.2f",Math.sqrt(Math.pow(a,2)+Math.pow(b,2)));
+    }
+}
