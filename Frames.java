9f273a2629aebf3ca417a48b25f515778d658601
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-06 11:33:32 +0530

    import java.util.*; public class Cost{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         int a=read.nextInt();         int b=read.nextInt();         int c=read.nextInt();         System.out.printf("%d",2*(a+b)*c);     } }

---
+++ Frames.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Cost{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        int a=read.nextInt();
+        int b=read.nextInt();
+        int c=read.nextInt();
+        System.out.printf("%d",2*(a+b)*c);
+    }
+}
