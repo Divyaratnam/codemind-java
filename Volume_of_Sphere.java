ffb5f45ded1fd9a9eac6fa52503d4f6ff8aa1383
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-06 14:38:15 +0530

    import java.util.*; public class Profit{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         double r=read.nextDouble();         System.out.printf("%.2f",(4*3.14*Math.pow(r,3)/3));     } }

---
+++ Volume_of_Sphere.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Profit{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        double r=read.nextDouble();
+        System.out.printf("%.2f",(4*3.14*Math.pow(r,3)/3));
+    }
+}
