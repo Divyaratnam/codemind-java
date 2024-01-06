7d9d5115945e5a76b455572204408091f31ef0d1
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-06 14:32:54 +0530

    import java.util.*; public class Profit{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         int s=read.nextInt();
            System.out.printf("%d %d",s*s,4*s);
        }
    }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Profit{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        int s=read.nextInt();
+        
+        System.out.printf("%d %d",s*s,4*s);
+    }
+}
