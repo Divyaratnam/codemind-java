d0001d7ae0dd3b572c353a35db4002245e82c7aa
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-06 14:25:15 +0530

    import java.util.*; public class King{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         int n=read.nextInt();         int m=read.nextInt();         System.out.printf("%d",(n*5+m*7));     } }

---
+++ King_Tours_.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class King{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        int n=read.nextInt();
+        int m=read.nextInt();
+        System.out.printf("%d",(n*5+m*7));
+    }
+}
