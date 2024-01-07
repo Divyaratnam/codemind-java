0e8dd1aba68fcf469b9844209441aacf53e1df8c
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-07 18:28:53 +0530

    import java.util.*; public class Weight{ public static void main(String[] args){     Scanner read=new Scanner(System.in);     int a=read.nextInt();     int b1=read.nextInt();     int b2=read.nextInt();     System.out.printf("%d",(3*a-b1-b2)); } }

---
+++ Average_weight.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Weight{
+public static void main(String[] args){
+    Scanner read=new Scanner(System.in);
+    int a=read.nextInt();
+    int b1=read.nextInt();
+    int b2=read.nextInt();
+    System.out.printf("%d",(3*a-b1-b2));
+}
+}
