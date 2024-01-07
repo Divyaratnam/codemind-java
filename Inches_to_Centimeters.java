1fcde9c44e763456275ad2b771997e828f8da57c
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-07 11:28:54 +0530

    import java.util.*; public class Inches{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         double a=read.nextDouble();         System.out.printf("%.2f",a*2.54);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Inches{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        double a=read.nextDouble();
+        System.out.printf("%.2f",a*2.54);
+    }
+}
