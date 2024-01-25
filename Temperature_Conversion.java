2cbc3c7d8697654b1757219e8ab8d75c891df8e3
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-25 21:07:48 +0530

    import java.util.*; public class Conversion{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         double c=read.nextInt();         System.out.printf("%.2f",32+(c*9/5));     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Conversion{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        double c=read.nextInt();
+        System.out.printf("%.2f",32+(c*9/5));
+    }
+}
