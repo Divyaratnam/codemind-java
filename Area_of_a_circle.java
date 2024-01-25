43792e7724bcd2b9281b69586cccc3645766aa53
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-25 21:09:33 +0530

    import java.util.*; public class Area{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         double r=read.nextInt();         System.out.printf("%.2f",3.14*r*r);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Area{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        double r=read.nextInt();
+        System.out.printf("%.2f",3.14*r*r);
+    }
+}
