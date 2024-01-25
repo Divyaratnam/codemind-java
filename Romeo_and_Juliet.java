1a8cc8cbdcf456d890ff29f2f8e2ad01cbabc64e
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-25 12:16:11 +0530

    import java.util.*; public class Romeo{  public static void main(String[] args){     Scanner read=new Scanner(System.in);     int x=read.nextInt();     int y=read.nextInt();     int z=read.nextInt();     System.out.printf("%d",(x*5+y*10)/z);  } }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Romeo{
+ public static void main(String[] args){
+    Scanner read=new Scanner(System.in);
+    int x=read.nextInt();
+    int y=read.nextInt();
+    int z=read.nextInt();
+    System.out.printf("%d",(x*5+y*10)/z);
+ }
+}
