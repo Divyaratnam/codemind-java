d9c644b84cd6a81f92c152bb462401dcae8f5b23
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-26 11:19:57 +0530

    import java.util.*; public class Ascii{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         char n=read.next().charAt(0);         System.out.printf("%d",(int)n);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Ascii{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        char n=read.next().charAt(0);
+        System.out.printf("%d",(int)n);
+    }
+}
