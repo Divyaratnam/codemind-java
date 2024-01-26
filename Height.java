fc54b1d80d1287f7822ac3e50636b91d7bcab32c
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-26 11:49:11 +0530

    import java.util.*; public class Height{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         int x=read.nextInt();         int y=read.nextInt();         if(x>=y)         System.out.printf("%d",x);         else         System.out.printf("%d",y);     } }

---
+++ Height.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Height{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        int x=read.nextInt();
+        int y=read.nextInt();
+        if(x>=y)
+        System.out.printf("%d",x);
+        else
+        System.out.printf("%d",y);
+    }
+}
