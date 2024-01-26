c64a835fbd45411eb90b0c07baba0d6cc088f84a
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-26 11:27:17 +0530

    import java.util.*; public class Modulus{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         int n=read.nextInt();         int m=read.nextInt();         System.out.printf("%d",n%m);     } }

---
+++ Modulus.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Modulus{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        int n=read.nextInt();
+        int m=read.nextInt();
+        System.out.printf("%d",n%m);
+    }
+}
