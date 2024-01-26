2cd5692f7739b37e687491713576d9ab2604a76b
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-26 11:53:15 +0530

    import java.util.*; public class ReachHome{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         int x=read.nextInt();         int y=read.nextInt();         if(x*5>=y)         System.out.print("YES");         else         System.out.print("NO");     } }

---
+++ Reach_Home.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ReachHome{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        int x=read.nextInt();
+        int y=read.nextInt();
+        if(x*5>=y)
+        System.out.print("YES");
+        else
+        System.out.print("NO");
+    }
+}
