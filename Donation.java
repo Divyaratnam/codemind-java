675dc132a35e637e5ce02c56ca7c1f1de1d743c1
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-26 14:10:59 +0530

    import java.util.*; public class Donation{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         int x=read.nextInt();         int y=read.nextInt();         System.out.printf("%d",y-x);     } }

---
+++ Donation.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Donation{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        int x=read.nextInt();
+        int y=read.nextInt();
+        System.out.printf("%d",y-x);
+    }
+}
