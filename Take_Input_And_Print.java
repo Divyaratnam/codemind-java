ff7ff9a8772800ac16d3944d21df463618a8e96c
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-26 11:29:28 +0530

    import java.util.*; public class InputPrint{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         String m=read.nextLine();         System.out.printf("%s",m);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class InputPrint{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        String m=read.nextLine();
+        System.out.printf("%s",m);
+    }
+}
