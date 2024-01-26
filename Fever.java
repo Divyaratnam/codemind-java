b1eb390a259ee6d04553c21dda25f667580d0cac
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2024-01-26 12:32:19 +0530

    import java.util.*; public class Fever{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         int x=read.nextInt();         if(x>98)         System.out.print("YES");         else         System.out.print("NO");     } }

---
+++ Fever.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Fever{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        int x=read.nextInt();
+        if(x>98)
+        System.out.print("YES");
+        else
+        System.out.print("NO");
+    }
+}
